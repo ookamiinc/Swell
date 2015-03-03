684e68f038c2a4d45b0ad640da2050886e3400c5
Masayuki Ono <mono0926@gmail.com>
2015-03-03 16:13:39 +0900

    Fix NSLog

--- Swell/LogLocation.swift
+++ Swell/LogLocation.swift
@@ -38,7 +38,7 @@ public class ConsoleLocation: LogLocation {
 
     public func log(message: @autoclosure() -> String) {
         if enabled {
-            NSLog(message())
+            NSLog("%@", message())
         }
     }
     
